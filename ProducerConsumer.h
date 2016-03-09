807f2c910a4f696fa52c395d3f177a8cfbee5ace
Andrii Bugaiov <andriibugaiov@gmail.com>
2016-03-08 21:58:54 -0500

    multiple prod/cons

--- ProducerConsumer.h
+++ ProducerConsumer.h
@@ -6,6 +6,7 @@
 #include <sstream>
 
 #include <queue>
+#include <vector>
 
 namespace ProducerConsumer {
 	class Item {
@@ -24,35 +25,52 @@ namespace ProducerConsumer {
 	class Storage {
 		std::queue<TItem *> items;
 		std::mutex mtx;
-		bool empty() {
-			return items.empty();
-		}
 	public:
-		bool get(TItem *&item) {
+		~Storage() {
+			Item *item = nullptr;
+			while (items.empty()) {
+				item = items.front();
+				items.pop();
+				delete item;
+				item = nullptr;
+			}
+		}
+		bool tryGet(TItem *&item) {
 			std::unique_lock<std::mutex> lck(mtx);
-			if (empty())
+			if (items.empty())
 				return false;
+			
 			item = items.front();
 			items.pop();
 			
 			std::ostringstream msg;
-			msg << "Item: "
-			<< item -> id
-			<< " *Consumed*"
-			<< std::endl;
+			msg << "Consumed: " << item -> id << std::endl;
 			std::cout << msg.str();
 			
 			return true;
+		}
+		void waitGet(TItem *&item) {
+			std::unique_lock<std::mutex> lck(mtx);
+			while (items.empty()) {
+				lck.unlock();
+				chrono::seconds secs(3);
+				std::this_thread::sleep_for(secs);
+				lck.lock();
+			}
+			
+			item = items.front();
+			items.pop();
+			
+			std::ostringstream msg;
+			msg << "Consumed: " << item -> id << std::endl;
+			std::cout << msg.str();
 		}
 		void put(TItem *item) {
 			std::unique_lock<std::mutex> lck(mtx);
 			items.push(item);
 			
 			std::ostringstream msg;
-			msg << "Item: "
-			<< item -> id
-			<< " *Produced*"
-			<< std::endl;
+			msg << "Produced: " << item -> id << std::endl;
 			std::cout << msg.str();
 		}
 	};
@@ -61,17 +79,18 @@ namespace ProducerConsumer {
 	class Worker {
 		std::thread job;
 	protected:
+		int id;
 		TStorage<TItem> &storage;
 		virtual void work() = 0;
 	public:
-		Worker(TStorage<TItem> &aStorage) : storage(aStorage) {
+		Worker(TStorage<TItem> &aStorage, int anId) : storage(aStorage), id(anId) {
 		}
 		void start() {
 			job = std::thread([this](){
 				work();
 			});
 		}
-		void wait () {
+		void wait() {
 			job.join();
 		}
 	};
@@ -79,51 +98,91 @@ namespace ProducerConsumer {
 	template<typename TItem, template<typename> class TStorage>
 	class Producer : public Worker<TItem, TStorage> {
 		int numToProd;
+		Item *produce() {
+			chrono::seconds secs(2);
+			std::this_thread::sleep_for(secs);
+			return Item::createItem();
+		}
 	protected:
 		virtual void work() {
 			for (int count = 0; count < numToProd; ++count) {
-				Item *item = Item::createItem();
-				
-				chrono::seconds secs(1);
-				std::this_thread::sleep_for(secs);
-				
-				this -> storage.put(item);
+				Item *item = produce();
+				if (item)
+					this -> storage.put(item);
 			}
 		}
 	public:
-		Producer(TStorage<TItem> &aStorage, int aNumToProd = 0) : Worker<TItem, TStorage>(aStorage), numToProd(aNumToProd) {}
+		Producer(TStorage<TItem> &aStorage, int anId, int aNumToProd = 0) : Worker<TItem, TStorage>(aStorage, anId), numToProd(aNumToProd) {}
 	};
 	
 	template<typename TItem, template<typename> class TStorage>
 	class Consumer : public Worker<TItem, TStorage> {
 		int numToCons;
+		void consume(Item *item) {
+			if (!item)
+				return;
+			
+			chrono::seconds secs(1);
+			std::this_thread::sleep_for(secs);
+			delete item;
+			item = nullptr;
+		}
 	protected:
 		virtual void work() {
 			for (int count = 0; count < numToCons;) {
 				TItem *item = nullptr;
-				if (this -> storage.get(item)) {
+				if (this -> storage.waitGet(item)) {
 					++count;
-					
-					chrono::seconds secs(2);
-					std::this_thread::sleep_for(secs);
-
-					delete item;
-					item = nullptr;
+					consume(item);
 				}
 			}
 		}
 	public:
-		Consumer(TStorage<TItem> &aStorage, int aNumToCons = 0) : Worker<TItem, TStorage>(aStorage), numToCons(aNumToCons) {}
+		Consumer(TStorage<TItem> &aStorage, int anId, int aNumToCons = 0) : Worker<TItem, TStorage>(aStorage, anId), numToCons(aNumToCons) {}
 	};
 	
 	void TestSuite() {
-		int numOfItems = 10;
 		Storage<Item> storage;
-		Producer<Item, Storage> producer(storage, numOfItems);
-		Consumer<Item, Storage> consumer(storage, numOfItems);
-		producer.start();
-		consumer.start();
-		producer.wait();
-		consumer.wait();
+		
+		std::vector<Producer<Item, Storage>> producers;
+		vector<int> pPlan = {4, 7, 3, 3};
+		for (int id = 0; id < pPlan.size(); ++id)
+			producers.push_back(Producer<Item, Storage>(storage, id, pPlan[id]));
+		
+		std::vector<Consumer<Item, Storage>> consumers;
+		vector<int> cPlan = {10, 17, 1, 1};
+		for (int id = 0; id < cPlan.size(); ++id)
+			consumers.push_back(Consumer<Item, Storage>(storage, id, cPlan[id]));
+
+		for (auto &prod : producers)
+			prod.start();
+		for (auto &cons : consumers)
+			cons.start();
+		for (auto &prod : producers)
+			prod.wait();
+		for (auto &cons : consumers)
+			cons.wait();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
